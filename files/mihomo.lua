9cc8a551339cff9b218cdc2b292df1520d3f9d2c
Jahid Hasan Shuvo <130297130+jahid421@users.noreply.github.com>
2026-07-26 23:15:40 +0600

    Update mihomo.lua

--- files/mihomo.lua
+++ files/mihomo.lua
@@ -1,6 +1,6 @@
 module("luci.controller.mihomo", package.seeall)
 
 function index()
-    entry({"admin", "services", "mihomo"}, alias("admin", "services", "mihomo", "main"), _("Mihomo"), 60)
-    entry({"admin", "services", "mihomo", "main"}, template("mihomo/main"), _("Mihomo"), 1).leaf = true
+    entry({"admin", "services", "mihomo"}, alias("admin", "services", "mihomo", "main"), _("DinoClash 🦕"), 60)
+    entry({"admin", "services", "mihomo", "main"}, template("mihomo/main"), _("DinoClash 🦕"), 1).leaf = true
 end
